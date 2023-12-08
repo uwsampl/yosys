d9aec272927f8c1699e69a28c18521fb10640e35
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2023-12-07 17:46:16 -0800

    Add simple mux test case - fails because of not

---
+++ backends/lakeroad/tests/simple-mux.sv
@@ -0,0 +1,14 @@
+// RUN: "$YOSYS" -p 'read_verilog -sv %s; prep -top test; pmuxtree; write_lakeroad'
+module test(input [2:0] a, output [2:0] out);
+  // assign out = ~ a;
+  always_comb begin
+	  case (a)
+		  2'b01: out = 2'b10;
+		  2'b00: out = 2'b10;
+		  2'b10: out = 2'b00;
+		  2'b11: out = 2'b01;
+		  default: out = 2'b01;
+	  endcase
+  end
+endmodule
+
